3f734e9ccc470a93637fb03e757861be917ff7f9
PhantomasKNB <heittbd@gmail.com>
2023-02-26 11:43:31 +0200

    first commit

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const word = document.querySelector(".text");
 const btn = document.querySelector(".btn");
 
 btn.addEventListener("click", () => {
-  word.innerHTML = "click";
+  word.innerHTML = "push";
   btn.innerHTML = "click";
 });
